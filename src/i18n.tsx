d2f11da2b3bda1ff1749b962dc1684aeb41be8ff
andrealiotta <andrea.liotta@ef.com>
2026-03-26 11:59:10 +0100

    refactor: remove browser language detection from i18n, URL is now source of truth

--- src/i18n.tsx
+++ src/i18n.tsx
@@ -1,41 +1,24 @@
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
 
-import translationEnglish from "./Translation/English/translation.json";
-import translationItalian from "./Translation/Italian/translation.json";
+import translationEnglish from './Translation/English/translation.json';
+import translationItalian from './Translation/Italian/translation.json';
 
 const resources = {
-    en: {
-        translation: translationEnglish,
-    },
-    it: {
-        translation: translationItalian,
-    },
-}
-
-// i18next.use(initReactI18next).init({
-//     resources, 
-//     lng: "it", // default language
-// });
-
-const DETECTION_OPTIONS = {
-    order: ['localStorage', 'navigator'],
-    caches: ['localStorage']
+  en: {
+    translation: translationEnglish,
+  },
+  it: {
+    translation: translationItalian,
+  },
 };
 
-i18next
-    .use(LanguageDetector)
-    .use(initReactI18next)
-    .init({
-        resources,
-        detection: DETECTION_OPTIONS,
-        fallbackLng: 'en',
-        interpolation: {
-            escapeValue: false,
-        },
-    });
-
-
+i18next.use(initReactI18next).init({
+  resources,
+  fallbackLng: 'it',
+  interpolation: {
+    escapeValue: false,
+  },
+});
 
 export default i18next;
